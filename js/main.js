1bce414ed3fe5b86a62e25571a6619c17bc916cc
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-10 17:01:12 -0500

    Finalicé la sección de productos, agregé HTML de la sección experiencias

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.slider-products').slick({
+        slidesToShow: 4,  // Muestra 4 productos por vez
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        dots: false, // si quieres puntitos abajo, ponlo en true
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: { slidesToShow: 3, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 600,
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 480,
+                settings: { slidesToShow: 1, slidesToScroll: 1 }
+            }
+        ]
+    });
+});
