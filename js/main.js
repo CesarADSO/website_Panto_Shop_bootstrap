969003bb8d94bdbc87c2b22af95a34fa7587892d
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-11 17:49:42 -0500

    Realicé un 50% del responsive de tablet de la tienda Panto

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         responsive: [
             {
                 breakpoint: 1024,
-                settings: { slidesToShow: 3, slidesToScroll: 1 }
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
             },
             {
                 breakpoint: 600,
