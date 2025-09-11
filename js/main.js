7bc862b5bbf439975fa57c65348687b533ddf281
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-11 16:04:33 -0500

    Realicé la sección reviews con estilos CSS

--- js/main.js
+++ js/main.js
@@ -22,3 +22,28 @@ $(document).ready(function () {
         ]
     });
 });
+
+$(document).ready(function () {
+    $('.slider-reviews').slick({
+        slidesToShow: 3,  // Muestra 3 productos por vez
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        dots: false, // si quieres puntitos abajo, ponlo en true
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: { slidesToShow: 3, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 600,
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 480,
+                settings: { slidesToShow: 1, slidesToScroll: 1 }
+            }
+        ]
+    });
+});
