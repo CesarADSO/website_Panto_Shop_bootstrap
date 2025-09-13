0398b31e2e4700d751492804325e4c2570f2f7c6
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-13 14:56:26 -0500

    Finalicé el responsive para tablet

--- js/main.js
+++ js/main.js
@@ -30,11 +30,11 @@ $(document).ready(function () {
         infinite: true,
         arrows: true,
         dots: false, // si quieres puntitos abajo, ponlo en true
-        autoplay: true,
+        autoplay: false,
         responsive: [
             {
                 breakpoint: 1024,
-                settings: { slidesToShow: 3, slidesToScroll: 1 }
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
             },
             {
                 breakpoint: 600,
